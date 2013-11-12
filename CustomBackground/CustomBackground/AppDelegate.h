54b749d363d078915e6e3503a3f251c141b171d1
Deepthi Tayi <deepthi.tayi@orionhealth.com>
2013-11-12 14:04:29 +1300

    Initial commit

---
+++ CustomBackground/CustomBackground/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CustomBackground
+//
+//  Created by Deepthi Tayi on 12/11/13.
+//  Copyright (c) 2013 D.T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CustomBackground/CustomBackground/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CustomBackground
+//
+//  Created by Deepthi Tayi on 12/11/13.
+//  Copyright (c) 2013 D.T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
